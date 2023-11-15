0fdd874551b288a5db6c3d9f2695a564d806af08
Ian Sundstrom <iwsundstrom@gmail.com>
2023-11-15 17:56:39 -0500

    Added extension methods for calculations involving Rects, Bounds, RectTransforms, and Screen Space. Also added basic animation methods for float such as EaseOut, Smoothstep, etc.

--- Runtime/ExtensionMethods.cs
+++ Runtime/ExtensionMethods.cs
@@ -516,6 +516,119 @@ namespace Buck
             return distance;
         }
 
+        
+
+        //*****[Rects, Bounds, RectTransforms, and Screen Space]*****
+        /// <summary>
+        /// Converts a RectTransform into a Bounds
+        /// </summary>
+        public static Bounds GetRectTransformBounds(RectTransform rectTransform)
+        {
+            Vector3 min = Vector3.positiveInfinity;
+            Vector3 max = Vector3.negativeInfinity;
+
+            // Get the 4 corners in world coordinates
+            Vector3[] rectTransformWorldCorners = new Vector3[4];
+            rectTransform.GetWorldCorners(rectTransformWorldCorners);
+
+            // Find the min and max corners
+            foreach (Vector3 v in rectTransformWorldCorners)
+            {
+                min = Vector3.Min(min, v);
+                max = Vector3.Max(max, v);
+            }
+
+            // Set the bounds using the min and max
+            Bounds bounds = new Bounds();
+            bounds.SetMinMax(min, max);
+            
+            return bounds;
+        }
+
+        /// <summary>
+        /// Creates a Rect that uses the min and max of the provided Bounds to define its area.
+        /// </summary>
+        /// <param name="bounds"></param>
+        /// <returns></returns>
+        public static Rect GetBoundsRectangle(Bounds bounds)
+            => Rect.MinMaxRect(bounds.min.x, bounds.min.y, bounds.max.x, bounds.max.y);
+
+
+
+        /// <summary>
+        /// Returns a Rect that represents the screen space position of the bounding box around a world space object.
+        /// Credit to YouTube user quill18creates in this video https://www.youtube.com/watch?v=2Tgqr1_ajqE
+        /// </summary>
+        public static Rect GetScreenRectangle(Renderer renderer, Camera camera = null)
+            => GetScreenRectangle(renderer.bounds, camera);
+        /// <summary>
+        /// Returns a Rect that represents the screen space position of the bounding box around a world space object.
+        /// Credit to YouTube user quill18creates in this video https://www.youtube.com/watch?v=2Tgqr1_ajqE
+        /// </summary>
+        public static Rect GetScreenRectangle(RectTransform rectTransform, Camera camera = null)
+            => GetScreenRectangle(GetRectTransformBounds(rectTransform), camera);
+
+        /// <summary>
+        /// Returns a Rect that represents the screen space position of the bounding box around a world space object.
+        /// Credit to YouTube user quill18creates in this video https://www.youtube.com/watch?v=2Tgqr1_ajqE
+        /// </summary>
+        public static Rect GetScreenRectangle(Bounds bounds, Camera camera = null)
+        {
+            if (camera == null)
+                return Rect.MinMaxRect(bounds.min.x, bounds.min.y, bounds.max.x, bounds.max.y);
+            
+            Vector3 c = bounds.center;
+            Vector3 e = bounds.extents;
+            Vector3[] screenSpaceCorners = new Vector3[8];
+
+            // For each of the 8 corners of our renderer's world space bounding box,
+            // convert those corners into screen space.
+            screenSpaceCorners[0] = camera.WorldToScreenPoint(new Vector3(c.x + e.x, c.y + e.y, c.z + e.z));
+            screenSpaceCorners[1] = camera.WorldToScreenPoint(new Vector3(c.x + e.x, c.y + e.y, c.z - e.z));
+            screenSpaceCorners[2] = camera.WorldToScreenPoint(new Vector3(c.x + e.x, c.y - e.y, c.z + e.z));
+            screenSpaceCorners[3] = camera.WorldToScreenPoint(new Vector3(c.x + e.x, c.y - e.y, c.z - e.z));
+            screenSpaceCorners[4] = camera.WorldToScreenPoint(new Vector3(c.x - e.x, c.y + e.y, c.z + e.z));
+            screenSpaceCorners[5] = camera.WorldToScreenPoint(new Vector3(c.x - e.x, c.y + e.y, c.z - e.z));
+            screenSpaceCorners[6] = camera.WorldToScreenPoint(new Vector3(c.x - e.x, c.y - e.y, c.z + e.z));
+            screenSpaceCorners[7] = camera.WorldToScreenPoint(new Vector3(c.x - e.x, c.y - e.y, c.z - e.z));
+
+            // Now find the min/max X & Y of these screen space corners.
+            
+            // Start by assuming the min and max is the first corner.
+            float minX = screenSpaceCorners[0].x;
+            float minY = screenSpaceCorners[0].y;
+            float maxX = screenSpaceCorners[0].x;
+            float maxY = screenSpaceCorners[0].y;
+
+            // Then, continue looping starting at 1 because the
+            // corner at index 0 is already accounted for.
+            for (int i = 1; i < 8; i++)
+            {
+                if(screenSpaceCorners[i].x < minX)
+                    minX = screenSpaceCorners[i].x;
+                
+                if(screenSpaceCorners[i].y < minY)
+                    minY = screenSpaceCorners[i].y;
+                
+                if(screenSpaceCorners[i].x > maxX)
+                    maxX = screenSpaceCorners[i].x;
+                
+                if(screenSpaceCorners[i].y > maxY)
+                    maxY = screenSpaceCorners[i].y;
+            }
+            
+            // Then return the Rect using these corners.
+            return Rect.MinMaxRect(minX, minY, maxX, maxY);
+        }
+
+
+        //*****[Float animation methods]*****
+        public static float EaseIn(float t) => 1f - Mathf.Cos(t * Mathf.PI * 0.5f);
+        public static float EaseOut(float t) => Mathf.Sin(t * Mathf.PI * 0.5f);
+        public static float Smoothstep(float t) => t * t * (3f - 2f * t);
+        public static float Smootherstep(float t) => t * t * t * (t * (6f*t - 15f) + 10f);
+
+
         #if UNITY_EDITOR
         /// <summary>
         /// Draws a text string in as a gizmo in world space. This only works in the Unity Editor.
