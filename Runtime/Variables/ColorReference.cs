91859b05ab25bd0bd94e17aec49d561b53d73f0c
Ian Sundstrom <iwsundstrom@gmail.com>
2023-11-16 11:05:20 -0500

    Added ColorVariable and ColorReference objects for UnityEngine.Color.

---
+++ Runtime/Variables/ColorReference.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Buck
+{
+    [Serializable]
+    public class ColorReference
+    {
+        public bool UseConstant = true;
+        public Color ConstantValue = Color.white;
+        public ColorVariable Variable;
+
+        public ColorReference()
+        { }
+
+        public ColorReference(Color value)
+        {
+            UseConstant = true;
+            ConstantValue = value;
+        }
+
+        public Color Value
+        {
+            get { return UseConstant ? ConstantValue : Variable.CurrentValue; }
+        }
+
+        public static implicit operator Color(ColorReference reference)
+        {
+            return reference.Value;
+        }
+    }
+}
---
+++ Runtime/Variables/ColorVariable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Buck
+{
+    [CreateAssetMenu(menuName = "BUCK/Variables/Color Variable")]
+    public class ColorVariable : BaseScriptableObject
+    {
+        public Color DefaultValue = Color.white;
+        
+        private Color currentValue;
+        public Color CurrentValue
+        {
+            get { return currentValue; }
+            set { currentValue = value;}
+        }
+
+        public void SetValue(Color value)
+        {
+            CurrentValue = value;
+        }
+
+        public void SetValue(ColorVariable value)
+        {
+            CurrentValue = value.CurrentValue;
+        }
+
+        private void OnEnable()
+        {
+            currentValue = DefaultValue;
+        }
+    }
+}
