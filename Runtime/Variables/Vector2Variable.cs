ec5b8d2153bfb425926f9504bce0eba213bc044f
Ian Sundstrom <iwsundstrom@gmail.com>
2023-11-16 11:10:53 -0500

    New Vector2Variable and Vector2Reference. Also property drawers for Vector2Reference and ColorReference.

---
+++ Runtime/Variables/Vector2Variable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Buck
+{
+    [CreateAssetMenu(menuName = "BUCK/Variables/Vector2 Variable")]
+    public class Vector2Variable : BaseScriptableObject
+    {
+        public Vector2 DefaultValue = Vector2.zero;
+        
+        private Vector2 currentValue;
+        public Vector2 CurrentValue
+        {
+            get { return currentValue; }
+            set { currentValue = value;}
+        }
+
+        public void SetValue(Vector2 value)
+        {
+            CurrentValue = value;
+        }
+
+        public void SetValue(Vector2Variable value)
+        {
+            CurrentValue = value.CurrentValue;
+        }
+
+        private void OnEnable()
+        {
+            currentValue = DefaultValue;
+        }
+    }
+}
