b5b7ca4dcf33daf90b5ec46f57acb00a7faff412
Ian Sundstrom <iwsundstrom@gmail.com>
2023-11-17 17:20:27 -0500

    Cleaned up individual ReferenceDrawers into one BaseReferenceDrawer.cs class and used inheritance to reuse code from there for all different variable types. Vector4ReferenceDrawer is the only one that needed its own unique script.

--- Runtime/Variables/Editor/Vector4ReferenceDrawer.cs
+++ Runtime/Variables/Editor/Vector4ReferenceDrawer.cs
@@ -5,71 +5,21 @@ using UnityEngine;
 namespace Buck
 {
     [CustomPropertyDrawer(typeof(Vector4Reference))]
-    public class Vector4ReferenceDrawer : PropertyDrawer
+    public class Vector4ReferenceDrawer : BaseReferenceDrawer
     {
-        /// <summary>
-        /// Options to display in the popup to select constant or variable.
-        /// </summary>
-        private readonly string[] popupOptions = 
-            { "Use Constant", "Use Variable" };
-
-        /// <summary> Cached style to use to draw the popup button. </summary>
-        private GUIStyle popupStyle;
-
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        protected override void CreateValueGUI(Rect position, SerializedProperty useConstant, SerializedProperty constantValue, SerializedProperty variable)
         {
-            if (popupStyle == null)
-            {
-                popupStyle = new GUIStyle(GUI.skin.GetStyle("PaneOptions"));
-                popupStyle.imagePosition = ImagePosition.ImageOnly;
-            }
-
-            label = EditorGUI.BeginProperty(position, label, property);
-            position = EditorGUI.PrefixLabel(position, label);
-            
-            EditorGUI.BeginChangeCheck();
-
-            // Get properties
-            SerializedProperty useConstant = property.FindPropertyRelative("UseConstant");
-            SerializedProperty constantValue = property.FindPropertyRelative("ConstantValue");
-            SerializedProperty variable = property.FindPropertyRelative("Variable");
-
-            // Calculate rect for configuration button
-            Rect buttonRect = new Rect(position);
-            buttonRect.yMin += popupStyle.margin.top;
-            buttonRect.width = popupStyle.fixedWidth + popupStyle.margin.right;
-            position.xMin = buttonRect.xMax;
-
-            // Store old indent level and set it to 0, the PrefixLabel takes care of it
-            int indent = EditorGUI.indentLevel;
-            EditorGUI.indentLevel = 0;
-
-            int result = EditorGUI.Popup(buttonRect, useConstant.boolValue ? 0 : 1, popupOptions, popupStyle);
-
-            useConstant.boolValue = result == 0;
-
-            
+            //Vector4 needs to overwrite how this created with GUI since by default unity will create Vector4 as a field where x,y,z,w are all on different lines.
+            //This puts them in a single line like how Vector3 acts.
             if (useConstant.boolValue)
             {
                 constantValue.vector4Value = EditorGUI.Vector4Field(position, "", constantValue.vector4Value); 
-                //EditorGUI.PropertyField(position, constantValue, GUIContent.none);
             }
             else
             {
                 EditorGUI.PropertyField(position, variable, GUIContent.none);
             }
             
-            /*
-            EditorGUI.PropertyField(position, 
-                useConstant.boolValue ? constantValue : variable, 
-                GUIContent.none);
-            */
-
-            if (EditorGUI.EndChangeCheck())
-                property.serializedObject.ApplyModifiedProperties();
-
-            EditorGUI.indentLevel = indent;
-            EditorGUI.EndProperty();
         }
     }
 }
