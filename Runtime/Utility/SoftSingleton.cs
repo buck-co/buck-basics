eacd28fc80e17958c60af40a5d2a90b2ff2cc7a5
Nick Pettit <nick@nickpettit.com>
2024-04-04 01:21:46 -0500

    Spelling and formatting updates.

--- Runtime/Utility/SoftSingleton.cs
+++ Runtime/Utility/SoftSingleton.cs
@@ -4,7 +4,7 @@ namespace Buck
 {
     /// <summary>
     /// An alternate singleton format that allows deleting and respawning new Singletons at runtime. Instead of auto generating whenever referenced like our default Singleton, 
-    /// it will  become the Instance from it's Awake method or a public call to SetAsSingleton(). This is better for Singletons that are unique to a single scene or other limited
+    /// it will become the Instance from it's Awake method or a public call to SetAsSingleton(). This is better for Singletons that are unique to a single scene or other limited
     /// period of the game's overall runtime, since this singleton does not lock out the possibility of a new Singleton ever taking over.
     /// Unlike Singleton<T>, it is possible for a reference to Instance to be null, so you must ensure that the SoftSingleton exists and is set up before using it.
     /// </summary>
@@ -15,19 +15,18 @@ namespace Buck
         [SerializeField] bool m_dontDestroyOnLoad = true;
 
         /// <summary>
-        /// Access singleton instance through this propriety.
+        /// Access singleton instance through this property.
         /// </summary>
-        public static T Instance => m_Instance;
+        public static T Instance
+            => m_Instance;
 
 
         protected virtual void Awake()
-        {
-            SetAsSingleton();
-        }
+            => SetAsSingleton();
 
         /// <summary>
         /// Public method for attempting to set the target SoftSingleton to the Instance. Will return true if it succeeds (no Instance already set) or false if it fails (Instance already filled).
-        /// SetAsSingleton() is called in SoftSingleton.Awake() as long as Awake isn't overrridden.
+        /// SetAsSingleton() is called in SoftSingleton.Awake() as long as Awake isn't overridden.
         /// </summary>
         public bool SetAsSingleton()
         {
